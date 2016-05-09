32d313c2184db14d28579eb521c44967da9fe342
Elias <elias.garcia@udc.es>
2016-05-09 11:56:53 +0200

    Ejercicio 9 terminado

---
+++ 09-anillo-cruzado-de-procesos/crossring.erl
@@ -0,0 +1,72 @@
+-module(crossring).
+
+%% PUBLIC API
+-export([start/3]).
+
+-define(CROSSRING, crossring).
+
+start(ProcNum, MsgNum, Message) when (ProcNum > 0), (MsgNum > 0) ->
+    true = register(?CROSSRING, spawn(fun() -> init_master(ProcNum-1, MsgNum, Message) end)),
+    ok.
+
+init_master(0, MsgNum, Message) ->
+    whereis(?CROSSRING) ! {MsgNum-1, Message, any},
+    loop(whereis(?CROSSRING));
+init_master(ProcNum, MsgNum, Message) ->
+    Half = ProcNum div 2,
+    Rest = ProcNum rem 2,
+    case {Half > 0, Half+Rest > 0} of
+        {true, true} ->
+            Left = spawn(fun() -> init(Half-1) end),
+            Right = spawn(fun() -> init(Half+Rest-1) end),
+
+            Left ! {MsgNum-1, Message, left},
+            loop_master(Left, Right);
+        {false, true} ->
+            Right = spawn(fun() -> init(Half+Rest-1) end),
+            Right ! {MsgNum-1, Message, any},
+            loop(Right)
+    end.
+
+init(0) ->
+    loop(whereis(?CROSSRING));
+init(ProcNum) ->
+    Next = spawn(fun() -> init(ProcNum-1) end),
+    loop(Next).
+
+loop_master(Left, Right) ->
+    receive
+        {0, _Message, _From} ->
+            Left ! stop,
+            Right ! stop,
+            unregister(?CROSSRING),
+            ok;
+        {MsgNum, Message, right} ->
+            Left ! {MsgNum-1, Message, left},
+            loop_master(Left, Right);
+        {MsgNum, Message, left} ->
+            Right ! {MsgNum-1, Message, right},
+            loop_master(Left, Right);
+        stop ->
+            Left ! stop,
+            Right ! stop,
+            unregister(?CROSSRING),
+            ok;
+        _Other ->
+            loop_master(Left, Right)
+    end.
+
+loop(Next) ->
+    receive
+        {0, _Message, _From} ->
+            Next ! stop,
+            ok;
+        {MsgNum, Message, From} ->
+            Next ! {MsgNum-1, Message, From},
+            loop(Next);
+        stop ->
+            catch Next ! stop,
+            ok;
+        _Other ->
+            loop(Next)
+    end.
